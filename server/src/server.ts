c382e96cfd6a31a5b340ad1898ae576b77199103
Bruno Duarte <brunosduarte@hotmail.com>
2022-11-06 23:10:56 -0300

    second class server finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import ShortUniqueId from 'short-unique-id'
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -19,10 +21,42 @@ async function bootstrap() {
     const count = await prisma.pool.count()
 
     return { count }
-
   })
 
-  await fastify.listen({ port: 3335 })
+  fastify.get('/users/count', async () => {
+    const count = await prisma.pool.count()
+
+    return { count }
+  })
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.pool.count()
+
+    return { count }
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code
+      }
+    })
+    
+
+    return reply.status(201).send({ code })
+  })
+
+  await fastify.listen({ port: 3335, /*host: '0.0.0.0'*/ })
 }
 
 bootstrap()
